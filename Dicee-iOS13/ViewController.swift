7201d6141c0dcaeaea0259324d8da21f19431549
Jorliman <jorliman@gmail.com>
2020-01-04 17:11:07 +0800

    Finish "Responding to User Interactions with IBActions"

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,18 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //    WHO       WHAT   VALUE
         diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
